903862ecd45733d5553325a5bddd22d2ebeb3289
mdomarad <michalunoo@gmail.com>
2023-10-08 16:09:50 +0200

    Initial commit

---
+++ src/LabGit1.java
@@ -0,0 +1,8 @@
+public class LabGit1 {
+    public static void main(String[] args) {
+
+        String name = "Michał";
+
+        System.out.println( "Cześć jestem " + name + " i to jest pierwsze labolatorium Git.");
+    }
+}
